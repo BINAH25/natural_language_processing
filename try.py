4ba4af952f35e5848131b677b0301e4c82b49b48
louis <visteen192@gmail.com>
2023-02-23 17:06:47 +0000

    gettting keyword and corresponding sentence

--- try.py
+++ try.py
@@ -1,4 +1,6 @@
 import pke
+from flashtext import KeywordProcessor
+from nltk.tokenize import sent_tokenize
 from summarizer import Summarizer
 import nltk
 import pprint
@@ -36,19 +38,38 @@ def get_nouns_multipartite(text):
     return out
 
 keywords = get_nouns_multipartite(full_text) 
-print (keywords)
-"""filtered_keys=[]
+#print (keywords)
+filtered_keys=[]
 for keyword in keywords:
     if keyword.lower() in summarized_text.lower():
         filtered_keys.append(keyword)
         
-print (filtered_keys)"""
+#print (filtered_keys)
 
-from nltk.tokenize import sent_tokenize
-from flashtext import KeywordProcessor
 def tokenize_sentences(text):
     sentences = [sent_tokenize(text)]
     sentences = [y for x in sentences for y in x]
     # Remove any short sentences less than 20 letters.
     sentences = [sentence.strip() for sentence in sentences if len(sentence) > 20]
     return sentences
+
+def get_sentences_for_keyword(keywords, sentences):
+    keyword_processor = KeywordProcessor()
+    keyword_sentences = {}
+    for word in keywords:
+        keyword_sentences[word] = []
+        keyword_processor.add_keyword(word)
+    for sentence in sentences:
+        keywords_found = keyword_processor.extract_keywords(sentence)
+        for key in keywords_found:
+            keyword_sentences[key].append(sentence)
+        for key in keyword_sentences.keys():
+            values = keyword_sentences[key]
+            values = sorted(values, key=len, reverse=True)
+            keyword_sentences[key] = values
+            return keyword_sentences
+        
+sentences = tokenize_sentences(summarized_text)
+keyword_sentence_mapping = get_sentences_for_keyword(filtered_keys, sentences)
+        
+print (keyword_sentence_mapping)
